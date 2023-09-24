dcb6b2fc7acc425738ce0b8c2fccbaec266a4b45
Yun Chung <yunxchung@gmail.com>
2023-09-25 00:51:47 +0900

    Add streamlit default port

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --gpus '"device=0"' --name pet-recog-demo -v `pwd`:/workspace nvcr.io/nvidia/pytorch:22.12-py3 /bin/bash
+docker run -it --gpus '"device=0"' --name pet-recog-demo -p 8501:8501 -v `pwd`:/workspace nvcr.io/nvidia/pytorch:22.12-py3 /bin/bash
